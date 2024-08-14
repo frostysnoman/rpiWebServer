a25c1ca17f6316c9f5b949f95423a557354f76b6
frostysnoman <frostysnoman@comcast.net>
2024-08-13 19:50:34 -0600

    updated batt

--- batt.py
+++ batt.py
@@ -23,29 +23,36 @@ def get_battdata():
 	batt_capacity = battdata['batt_capacity']
 	conn.close()
 	if battdata is None:
+		print('no battdata')
 		abort(404)
 	return ([batt_current, batt_voltage, batt_rem_charge, batt_capacity])
 	
 def put_battdata(batt_data):
 	conn = get_db_connection()
 	print(batt_data)
-	batt_current = batt_data["current"]
-	batt_voltage = batt_data["voltage"]
-	batt_rem_charge = batt_data["remaining_charge"]
-	batt_capacity = batt_data["capacity"]
+	print(type(batt_data))
+	print('putbattdata')
+	if 'current' in batt_data:
+		batt_current = batt_data['current']
+	batt_voltage = batt_data['voltage']
+	batt_rem_charge = batt_data['remaining_charge']
+	batt_capacity = batt_data['capacity']
 	conn.execute('INSERT INTO battdata (batt_current, batt_voltage, batt_rem_charge, batt_capacity) VALUES (?, ?, ?, ?)', (batt_current, batt_voltage, batt_rem_charge, batt_capacity))
 	conn.commit()
 	print('wrote to db')
-	batt_data = get_battdata()
-	bcurrent = batt_data[0]
-	bvoltage = batt_data[1]
-	brc = batt_data[2]
-	bcap = batt_data[3]
+	batt_data2 = get_battdata()
+	print('batt_data_type',type(batt_data2))
+	bcurrent = batt_data2[0]
+	bvoltage = batt_data2[1]
+	brc = batt_data2[2]
+	bcap = batt_data2[3]
 	actuator = 'charger'
-	if brc < 80:	
+	if brc < 80:
+		print('Current charge is : ',brc)
 		kasarun = "kasa --host 192.168.150.211 on --name "+actuator
 		subprocess.run(kasarun.split())
 	else:
+		print('Current charge is :',brc)
 		kasarun = "kasa --host 192.168.150.211 off --name "+actuator
 		subprocess.run(kasarun.split())
 	pass
@@ -63,6 +70,8 @@ config.read(config_path)
 # the callback func when you receive data
 def on_data_received(client, data):
 	filtered_data = Utils.filter_fields(data, config['data']['fields'])
+	print('odr')
+	print(filtered_data)
 	put_battdata(filtered_data)
 	
 	print('finished odr')
@@ -70,6 +79,7 @@ def on_data_received(client, data):
 	pass
 	
 print(current_date_and_time)
+print('calling client')
 BatteryClient(config, on_data_received).connect()
 dbus.connection.close()
 print('finished')

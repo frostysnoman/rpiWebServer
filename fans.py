3f5bc71ebb35276b2cec3eaf8d8dbf6e456cb86b
frostysnoman <frostysnoman@comcast.net>
2024-08-13 20:34:04 -0600

    updated fans to run over 13v'

--- fans.py
+++ fans.py
@@ -72,28 +72,34 @@ relay5b = 24
 relay6b = 4
 relay7b = 3
 relay8b = 2
-
+fan1 = 14
+fan2 = 12
 relayNO = GPIO.LOW
 relayNC = GPIO.HIGH
-   
 
-GPIO.setup(relay5b, GPIO.OUT, initial=relayNO)   
-GPIO.setup(relay6b, GPIO.OUT, initial=relayNO)   
-GPIO.setup(relay7b, GPIO.OUT, initial=relayNO)  
+GPIO.setup(relay5b, GPIO.OUT, initial=relayNO)
+GPIO.setup(relay6b, GPIO.OUT, initial=relayNO)
+GPIO.setup(relay7b, GPIO.OUT, initial=relayNO)
 GPIO.setup(relay8b, GPIO.OUT, initial=relayNO)
-
+GPIO.setup(fan1, GPIO.OUT, initial=relayNO)
+GPIO.setup(fan2, GPIO.OUT, initial=relayNO)
 
 relay5bSts = GPIO.input(relay5b)
 relay6bSts = GPIO.input(relay6b)
 relay7bSts = GPIO.input(relay7b)
 relay8bSts = GPIO.input(relay8b)
-
+fan1Sts = GPIO.input(fan1)
+fan2Sts = GPIO.input(fan2)
 print("East Underground Fan is ", relay8bSts)
 print("West Underground Fan is ", relay7bSts)
 print("South Turbulator is ", relay6bSts)
 print("North Turbulator is ", relay5bSts)
+print("cpuFan1 is ",fan1Sts)
+print("cpuFan2 is ",fan2Sts)
+GPIO.output(fan1, GPIO.HIGH)
+GPIO.output(fan2, GPIO.HIGH)
 
-if raddata > 100 and  8 <= current_date_and_time.hour <= 17 and bvoltage > 12: # water between 9am and 3pm
+if raddata > 100 and  8 <= current_date_and_time.hour <= 17 and bvoltage > 13: # water between 9am and 3pm
 	GPIO.output(relay7b, GPIO.HIGH)
 	GPIO.output(relay6b, GPIO.HIGH)
 	GPIO.output(relay5b, GPIO.HIGH)
@@ -129,10 +135,14 @@ relay5bSts = GPIO.input(relay5b)
 relay6bSts = GPIO.input(relay6b)
 relay7bSts = GPIO.input(relay7b)
 relay8bSts = GPIO.input(relay8b)
-
+fan1Sts = GPIO.input(fan1)
+fan2Sts = GPIO.input(fan2)
 print("East Underground Fan is ", relay8bSts)
 print("West Underground Fan is ", relay7bSts)
 print("South Turbulator is ", relay6bSts)
-print("North Turbulator is ", relay5bSts)	
+print("North Turbulator is ", relay5bSts)
+print("CPU Fan1 is ", fan1Sts)
+print("CPU Fan2 is ", fan2Sts)
+
 #time.sleep(3500)
 sys.exit()
